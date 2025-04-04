29019253912f25df9119b1125b68cbacde4cfbbf
amrah072207 <fathimaamrahimran@gmail.com>
2025-04-05 03:18:25 +0530

    Added hero slides and made a carousel on the homepage, which slides automatically every 3 seonds. also added custom fonts for the project (mont serrat variations).

---
+++ lib/components/productcard.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+
+class Productcard extends StatelessWidget {
+  final Product
+  const Productcard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: unused_import, sized_box_for_whitespace, avoid_print, unused_label, dead_code
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:project/components/bottombar.dart';
 import 'package:project/components/custombar.dart';
@@ -12,6 +14,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomepageState extends State<HomePage> {
+  final PageController _pageController=PageController();
+  int currentpage=0;
+  final List<String> heroslides=[
+    "assets/heroslides/slide1.png",
+    "assets/heroslides/slide2.png",
+    "assets/heroslides/slide3.png",
+    "assets/heroslides/slide4.png"
+  ];
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 3), (Timer timer)
+    {
+      if(currentpage<heroslides.length-1){
+        currentpage++;
+      }else{
+        currentpage=0;
+      }
+
+      if(_pageController.hasClients){
+        _pageController.animateToPage(currentpage, duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
+      }
+  });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return 
@@ -19,8 +47,40 @@ class _HomepageState extends State<HomePage> {
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: const CustomBar(),
-        body: Center(child: Text("Content below AppBar")),
-        bottomNavigationBar: Bottombar()
+        bottomNavigationBar: Bottombar(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 220,
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: heroslides.length,
+                onPageChanged: (index) {
+                  currentpage = index;
+                },
+                itemBuilder: (context,index)
+                {
+                  return Padding(padding: EdgeInsets.fromLTRB(12, 4, 12, 4),
+                  child: ClipRRect(
+                    child: Image.asset(heroslides[index],
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    ),
+                  ),
+                  
+                  );
+                }
+                ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left:12.0),
+              child: Text("Vintage Collection",style: TextStyle(color: Color(0xFF7dadc4),fontSize: 21,fontFamily: 'MontserratMedium' ),),
+            )
+
+          ],
+        ),
       
           
         ),
